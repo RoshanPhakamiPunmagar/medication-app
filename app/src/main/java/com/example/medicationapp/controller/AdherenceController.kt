0ff80d043129519ab39cef9321f333c02a2b2754
Roshan Magar <rajuphakami2020@gmail.com>
2025-04-24 20:13:16 +1000

    Just commented on AdherenceController

--- app/src/main/java/com/example/medicationapp/controller/AdherenceController.kt
+++ app/src/main/java/com/example/medicationapp/controller/AdherenceController.kt
@@ -3,7 +3,7 @@ package com.example.medicationapp.controller
 import android.content.Context
 import com.example.medicationapp.database.AppDatabase
 import com.example.medicationapp.model.AdherenceLog
-
+//This is AdherenceController class
 class AdherenceController(context: Context) {
     private val adherenceDao = AppDatabase.getDatabase(context).adherenceLogDao()
 

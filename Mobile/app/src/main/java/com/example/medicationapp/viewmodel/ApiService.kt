961a6280d864b9979f41742d6f3bb8bdcc50b9ba
Roshan Magar <rajuphakami2020@gmail.com>
2025-05-26 17:01:32 +1000

    Refractor code of ApiService and App properties

--- Mobile/app/src/main/java/com/example/medicationapp/viewmodel/ApiService.kt
+++ Mobile/app/src/main/java/com/example/medicationapp/viewmodel/ApiService.kt
@@ -1,6 +1,5 @@
 package com.example.medicationapp.viewmodel
 
-import com.example.medicationapp.model.AiAnalysisResponse
 import com.example.medicationapp.model.Client
 import com.example.medicationapp.model.ClientMedication
 import com.example.medicationapp.model.ClientMedsDescriptions
@@ -8,12 +7,8 @@ import com.example.medicationapp.model.ClientWithMedicationsDTO
 import com.example.medicationapp.model.LoginRequest
 import com.example.medicationapp.model.Medication
 import com.example.medicationapp.model.User
-import com.example.medicationapp.model.dto.AdherenceLogDTO
-import com.example.medicationapp.model.dto.ClientMedicationDTO
-import com.example.medicationapp.model.dto.MedicationLogDTO
 import okhttp3.ResponseBody
 import retrofit2.Call
-import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
@@ -25,7 +20,7 @@ import retrofit2.http.Query
 
 interface ApiService {
 
-    @GET("api/clients")
+    @GET("https://medication-app-deployment.onrender.com/api/clients")
     fun getClientsPaged(
         @Query("page") page: Int,
         @Query("size") size: Int
@@ -33,67 +28,49 @@ interface ApiService {
 
 
 
-    @POST("mobile/check")
+    @POST("https://medication-app-deployment.onrender.com/mobile/check")
     fun login(
         @Body request: LoginRequest
     ): Call<Map<String, @JvmSuppressWildcards Any>>
 
-    @POST("mobile/user")
+    @POST("https://medication-app-deployment.onrender.com/mobile/user")
     fun register(
         @Body user: User
     ): Call<Map<String, String>>
 
-    @GET("meds/one/get/{medsId}")
-    fun getMeds(
-        @Path("medsId") medsId: Long
-    ): Call<String>
 
-
-    @GET("mobile/userCarer")
+    @GET("https://medication-app-deployment.onrender.com/mobile/userCarer")
     fun getAllCarers(): Call<List<User>>
 
-    @GET("meds/details")
-    fun getMedicationWithName(@Query("medicationList") list: List<Long>): Call<ClientMedsDescriptions>
+    @GET("https://medication-app-deployment.onrender.com/meds/details")
+    fun getMedicationWithName(@Query("medicationList") list: List<String>): Call<ClientMedsDescriptions>
 
-    @GET("api/medications")
+    @GET("https://medication-app-deployment.onrender.com/api/medications")
     fun getAllMedications(): Call<List<Medication>>
 
-    @GET("api/clients")
+    @GET("https://medication-app-deployment.onrender.com/api/clients")
     fun getAllClients(): Call<List<Client>>
 
-    @POST("api/medication/assign")
-    fun assignMedication(@Body dto: ClientMedicationDTO): Call<ResponseBody>
+    @POST("https://medication-app-deployment.onrender.com/api/medication/assign")
+    fun assignMedication(@Body dto: ClientMedication): Call<ResponseBody>
 
-    @POST("mobile/assignCarerToClient")
+    @POST("https://medication-app-deployment.onrender.com/mobile/assignCarerToClient")
     @FormUrlEncoded
     fun assignCarerToClient(
         @Field("clientId") clientId: Long,
         @Field("carerUserId") carerUserId: Long
     ): Call<Map<String, String>>
 
-    @GET("api/medication/clients-with-medications/{carerId}")
+    @GET("https://medication-app-deployment.onrender.com/api/medication/clients-with-medications/{carerId}")
     fun getClientsWithMedications(
         @Path("carerId") carerId: Long
     ): Call<List<ClientWithMedicationsDTO>>
 
-    @GET("api/medication/get/{carerId}")
-    fun getClientsMedicationOfLoggedUser(
-        @Path("carerId") carerId: Long
-    ): Call<List<ClientMedicationDTO>>
-
-    @POST("mobile/removeCarerFromClient")
+    @POST("https://medication-app-deployment.onrender.com/mobile/removeCarerFromClient")
     @FormUrlEncoded
     fun removeCarerFromClient(
         @Field("clientId") clientId: Long
     ): Call<Map<String, String>>
 
-    @GET("logs/get/{id}")
-    fun getAdherenceLogs(@Path("id") id : Long): Call<List<AdherenceLogDTO>>
-
-    @GET("logs/get/ai/{patientId}")
-    fun getAiAnalysis(@Path("patientId") patientId : Long): Call<AiAnalysisResponse>
-
-    @POST("logs/post/log")
-    suspend fun postMedicationLog(@Body dto: MedicationLogDTO): Response<Unit>
 
 }
